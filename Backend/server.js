57c5df11aba558d49f9b92999deb15e4f2d6f2b4
Hitomi sand <Felipe.navarro.godoy@gmail.com>
2026-05-25 10:28:04 -0300

    Add files via upload

---
+++ Backend/server.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+// banco de dados fake
+let usuarios = [
+  { id: 1, nome: "João", email: "joao@gmail.com" },
+  { id: 2, nome: "Maria", email: "maria@gmail.com" },
+  { id: 3, nome: "Carlos", email: "carlos@gmail.com" },
+];
+// get Pag inicial
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando com sucesso!");
+});
+// get lista de usuarios
+app.get("/usuarios", (req, res) => {
+  res.json(usuarios);
+});
+// get com parametro ("filtro")
+app.get("/usuario/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const usuario = usuarios.find((u) => u.id === id);
+  if (!usuario) {
+    return res.status(404).json({
+      mensagem: "Usuário não encotrado",
+    });
+  }
+  res.json(usuario);
+});
+
+//post - cadastrar usuario
+// post cadastrar usuario
+app.post("/cadastro", (req, res) => {
+  const { nome, email } = req.body;
+  if (!nome || !email) {
+    return res.status(400).json({
+      mensagem: "Nome e email são obrigatórios",
+    });
+  }
+  const novoUsuario = {
+    id: usuarios.length + 1,
+    nome,
+    email,
+  };
+  usuarios.push(novoUsuario);
+
+  res.status(201).json({
+    mensagem: "Cadastro realizado com sucesso!",
+    usuario: novoUsuario,
+  });
+});
+// Put editar usuario
+app.put("/editar/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const { nome, email } = req.body;
+  const usuario = usuarios.find((u) => u.id === id);
+  if (!usuario) {
+    return res.status(404).json({
+      mensagem: "Usuário não encontrado",
+    });
+  }
+  if (nome) usuario.nome = nome; // criei
+  if (email) usuario.email = email; // criei
+  res.json({
+    mensagem: "Usuário atualizado com sucesso!",
+    usuario,
+  });
+});
+// Delete deletar usuário
+app.delete("/deletar/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const index = usuarios.findIndex((u) => u.id === id);
+  if (index === -1) {
+    return res.status(404).json({
+      mensagem: "Usuário não encontrado",
+    });
+  }
+  const usuarioRemovido = usuarios.splice(index, 1);
+  res.json({
+    mensagem: "Usuario deletado com sucesso!",
+    usuario: usuarioRemovido[0],
+  });
+});
+// Servidor
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost: ${PORT}`); //<-- crase
+});
